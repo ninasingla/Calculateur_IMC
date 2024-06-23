b40c8bc16bd3ed2cda5ed0f1751c762f97849814
Nina <ninasingla@me.com>
2024-06-23 16:31:55 +0200

    Ajout des inputs nécessaire au calculateur

---
+++ imc.py
@@ -0,0 +1,11 @@
+# CALCULATEUR D'IMC 
+# IMC = Poids / Taille²
+
+print("Calculateur d'IMC :")
+
+prenom = input("Votre prénom : ")
+taille = input("Votre taille (en cm) : ")
+poids = input("Votre poids (en kg) : ")
+
+
+
